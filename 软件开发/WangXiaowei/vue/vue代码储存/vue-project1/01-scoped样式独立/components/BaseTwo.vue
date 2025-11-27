ea04dd1327e627c601c2431d15209cf84b190eac
Mengyan153 <2678527153@qq.com>
2025-11-27 10:03:49 +0800

    add vue2

---
+++ 软件开发/WangXiaowei/vue/vue代码储存/vue-project1/01-scoped样式独立/components/BaseTwo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>BaseTwovue</div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+    div{
+        border: 2px solid orange;
+        margin: 30px;
+    }
+
+</style>
---
+++ 软件开发/WangXiaowei/vue/vue代码储存/vue-project1/11-封装表单类组件/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <BaseSelect
+      :cityId="selectId"
+    >
+     </BaseSelect>
+  </div>
+</template>
+
+<script>
+import BaseSelect from "./components/BaseSelect.vue";
+export default {
+  data() {
+    return {
+      selectId: "103",
+    };
+  },
+  components: {
+    BaseSelect,
+  },
+};
+</script>
+
+<style>
+</style>
